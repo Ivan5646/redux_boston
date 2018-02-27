14fb64e78a023020952f96db9c949ee51500b2da
Ivan5646 <Mike000027@yandex.ru>
2018-02-27 23:57:44 +0700

    reducer error exoport fix

--- dev/js/reducers/index.js
+++ dev/js/reducers/index.js
@@ -1,6 +1,8 @@
 import {combineReducers} from "redux";
 import UserReducer from "./reducer-users"; // UserReducer - the name of the variable that holds reducer-users
 
-export default const allReducers = combineReducers({ // that will be thrown to the Store
+const allReducers = combineReducers({ // that will be thrown to the Store
     users: UserReducer // equals to object in reduser-users
 });
+
+export default allReducers;
