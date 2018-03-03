183db36f26a84e2069e256e4db914dbb830e8c64
Ivan5646 <Mike000027@yandex.ru>
2018-03-03 18:26:35 +0700

    created the container

--- dev/js/containers/user-list.js
+++ dev/js/containers/user-list.js
@@ -14,4 +14,10 @@ class UserList extends Component {
     }
 }
 
-export default UserList;
+function mapStateToProps(state){ //takes a piece of state which is part of the Store and sends it to the Component as props
+    return {
+        users: state.users
+    };
+}
+
+export default connect(mapStateToProps)(UserList); // this is now a contanier
