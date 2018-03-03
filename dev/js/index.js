0dbea8d69999e84452cca528f97ae14fb7b39069
Ivan5646 <Mike000027@yandex.ru>
2018-03-03 17:19:09 +0700

    provider finished

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,7 +1,7 @@
 import 'babel-polyfill'; 
 import React from 'react';
 import ReactDOM from "react-dom"; // all above imported from node modules
-import Provider from 'react-redux';
+import {Provider} from 'react-redux';
 import {createStore} from "redux"; // {} means import a var or function from package
 import allReducers from "./reducers";
 import App from './components/app';
@@ -9,6 +9,8 @@ import App from './components/app';
 const store = createStore(allReducers);
 
 ReactDOM.render(
-    <App/>,
+    <Provider store={store}>
+        <App/>
+    </Provider>,
     document.getElementById('root')
 );
