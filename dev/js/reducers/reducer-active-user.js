23082304c31877ff7a2af33d71faf82b5ac72cae
Ivan5646 <Mike000027@yandex.ru>
2018-03-04 23:43:27 +0700

    created displaying active user

--- dev/js/reducers/reducer-active-user.js
+++ dev/js/reducers/reducer-active-user.js
@@ -1,7 +1,7 @@
 // this code below gonna listen to selectUser action
 // by "default" all actions are sent to reducers/index.js allReducers
 
-export default function(state=null, action) {
+export default function(state={}, action) {
     switch(action.type) {
         case "USER_SELECTED":
         return action.payload;
