3fc9c05e51aec0aca850be7ac7b443bb7ef9a21e
Ivan5646 <Mike000027@yandex.ru>
2018-03-04 23:12:00 +0700

    created reducer-active-user

--- dev/js/reducers/index.js
+++ dev/js/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from "redux";
 import UserReducer from "./reducer-users"; // UserReducer - the name of the variable that holds reducer-users
+import ActiveUserReducer from "./reducer-active-user";
 
 const allReducers = combineReducers({ // that will be thrown to the Store
-    users: UserReducer // equals to object in reduser-users
+    users: UserReducer, // equals to object in reducer-users
+    activeUser: ActiveUserReducer
 });
 
 export default allReducers;
