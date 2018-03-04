557feb7898269b0b6353ff674c816135c82958eb
Ivan5646 <Mike000027@yandex.ru>
2018-03-04 22:35:50 +0700

    created action onCLick comments

--- dev/js/actions/index.js
+++ dev/js/actions/index.js
@@ -1,6 +1,6 @@
-export const selectUser = (user) => {
+export const selectUser = (user) => { // this is an Action Creator
     console.log("You clicked on user: ", user.first);
-    return {
+    return { // this is the action itself or object
         type: "USER_SELECTED",
         payload: user
     }
