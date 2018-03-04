e9981e393fb0b6cf645d831d03c7832c12bdc4f6
Ivan5646 <Mike000027@yandex.ru>
2018-03-04 23:50:19 +0700

    active user, null state handling

--- dev/js/reducers/reducer-active-user.js
+++ dev/js/reducers/reducer-active-user.js
@@ -1,7 +1,7 @@
 // this code below gonna listen to selectUser action
 // by "default" all actions are sent to reducers/index.js allReducers
 
-export default function(state={}, action) {
+export default function(state=null, action) {
     switch(action.type) {
         case "USER_SELECTED":
         return action.payload;
