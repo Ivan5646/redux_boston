3f6f3adcd2743c23dbca34dc8fb9a0e90d168273
Ivan5646 <Mike000027@yandex.ru>
2018-03-25 03:52:38 +0700

    my reducer2

--- dev/js/reducers/index.js
+++ dev/js/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from "redux";
 import UserReducer from "./reducer-users"; // UserReducer - the name of the variable that holds reducer-users
 import ActiveUserReducer from "./reducer-active-user";
+import Kitties from "./reducer-kitties";
 
 const allReducers = combineReducers({ // that will be thrown to the Store
     users: UserReducer, // equals to object in reducer-users
-    activeUser: ActiveUserReducer
+    activeUser: ActiveUserReducer,
+    kitties: Kitties
 });
 
 export default allReducers;
---
+++ dev/js/reducers/reducer-kitties.js
@@ -0,0 +1,21 @@
+// function that returns the part of the app responsible for storing the users
+
+export default function(){
+    return [
+        {
+            id: "kitty1",
+            name: "Snowball",
+            color: "white"
+        },
+        {
+            id: "kitty2",
+            name: "Mr. Blonde",
+            color: "with stripes"
+        },
+        {
+            id: "kitty3",
+            name: "Biscuit",
+            color: "creamy"
+        }
+    ]
+}
