efcb7c41fe732933b44f62de44721170d98d9e3e
Ivan5646 <Mike000027@yandex.ru>
2018-03-27 01:02:18 +0700

    clean-up2

--- dev/js/actions/index.js
+++ dev/js/actions/index.js
@@ -1,5 +1,5 @@
 export const selectUser = (chlen) => { // this is an Action Creator. How the fuck the argument is referred to user object?
-    console.log("You clicked on user: ", chlen.chlen);
+    console.log("You clicked on user: ", chlen.first);
     return { // this is the action itself or object
         type: "USER_SELECTED", // wtf is this?
         payload: chlen
